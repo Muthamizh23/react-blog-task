8e00308dd368b46f286ad18ab1a597bea7dae571
Muthamizh23 <158249272+Muthamizh23@users.noreply.github.com>
2024-04-13 11:36:40 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Card.css"
+
+const Header= () => {
+    return (
+        <div>
+             <header className="head-box">
+                
+                    <div className="contanier">
+                        <h1 className="head-head">EDU-TECH</h1>
+                        <p className="head-para">GET START JOURNEY WITH US...!!</p>
+                    </div>
+                
+            </header>           
+        </div>
+    );
+};
+
+export default Header;
